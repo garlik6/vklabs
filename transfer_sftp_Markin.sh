53c8a278f37f8945d055236fdfed3ca80e1a8e21
SergeyM <sergey-markin2013@mail.ru>
2023-03-28 10:25:01 +0300

    fix naming errors

--- transfer_sftp_Markin.sh
+++ transfer_sftp_Markin.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-
 LOCAL_DIR="/var/log"
 PATH_TO_SSH="/home/user/.ssh/id_rsa" #change to current pc
 
 # number of files to create
 NUM_FILES=5
 
-# maximum age of files in seconds (1 week)
-MAX_AGE=$((60)) # 604800 - one week in seconds
+# maximum age of files in minutes (1 week)
+MAX_AGE=2 # 10080 - one week in minutes 
 
 REMOTE_HOST="10.169.0.137" #change to "192.168.2.78"
 REMOTE_USER="osboxes" #change to "admini"
-REMOTE_DIR="logs" #create dir on remote server
+REMOTE_DIR="/home/$REMOTE_USER/logs" # create logs dir on remote server
 
 # loop for create log files
 for i in $(seq 1 $NUM_FILES); do
     FILENAME=$LOCAL_DIR/"logfile_$i"$(date +"_%Y-%m-%d-%H:%M:%S").log
-    echo "VK SRE log file number $i" >> "$FILENAME"
-done
-
-# Transfer files to remote server using SFTP
-sftp -i $PATH_TO_SSH "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR" <<EOF
-put $LOCAL_DIR/*.log
-exit
+    echo "VK SRE log file number $i %H:%M:%S" >> "$FILENAME"
+    
+    # Transfer files to remote server using SFTP
+    sftp -i $PATH_TO_SSH "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR" <<EOF
+    put $FILENAME
+    exit
 EOF
 
+done
+
 # Remove files older than the maximum age on the remote server
-ssh -i $PATH_TO_SSH "$REMOTE_USER@$REMOTE_HOST" "find $REMOTE_DIR/*.log -mtime +$(($MAX_AGE/86400)) -delete"
+# files passed as arguments via {} and \;  thats indicates the rm command should be executed for each found file separately
+ssh -i $PATH_TO_SSH "$REMOTE_USER@$REMOTE_HOST" "find $REMOTE_DIR/logfile_*.log -type f -mmin +$MAX_AGE -exec rm {} \;"
 
